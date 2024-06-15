7fee91fb68174e854bbbb51d602c0aa83b4994c6
EMANUEL-KRASS-DE-SOUZA <emanuelkraussdesouza@gmail.com>
2024-06-15 17:56:06 -0300

    Criação da SideBar + Criação do Carrinho

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,10 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'ProjetoAluguel_FrontEnd_2024';
+ 
+  sidebarWidth: number = 250;
+
+  toggleSidebar() {
+    this.sidebarWidth = this.sidebarWidth === 150 ? 0 : 2500; // Alternar entre 0 (retrair) e 250 (expandir)
+  }
 }
