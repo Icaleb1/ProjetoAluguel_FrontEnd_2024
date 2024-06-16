dadb160aa2470b4f3d5897d5acec3d719643c789
Icaleb1 <calebfernandes080@gamil.com>
2024-06-16 18:20:54 -0300

    Método pra adicionar itens ao carrinho;

--- src/app/brinquedos/brinquedo-listagem/brinquedo-listagem.component.ts
+++ src/app/brinquedos/brinquedo-listagem/brinquedo-listagem.component.ts
@@ -1,8 +1,11 @@
+import { ItemCarrinhosService } from './../../shared/service/item-carrinho/item-carrinhos.service';
 import { BrinquedosService } from '../../shared/service/brinquedo/brinquedos.service';
 import { Component, OnInit } from '@angular/core';
 import { Brinquedo } from '../../shared/model/brinquedo';
 import { ActivatedRoute, Router } from '@angular/router';
 import { BrinquedoSeletor } from '../../shared/model/seletor/brinquedoSeletor';
+import { CarrinhosService } from '../../shared/service/carrinho/carrinhos.service';
+import { ItemCarrinho } from '../../shared/model/itemCarrinho';
 
 
 @Component({
@@ -16,6 +19,7 @@ export class BrinquedoListagemComponent implements OnInit{
   public seletor: BrinquedoSeletor = new BrinquedoSeletor();
 
   constructor(private brinquedosService : BrinquedosService,
+              private itemCarrinhosService : ItemCarrinhosService,
               private router: Router,
               private route: ActivatedRoute
   ){}
@@ -50,4 +54,21 @@ export class BrinquedoListagemComponent implements OnInit{
     );
   }
 
+  public adicionarItemAoCarrinho(brinquedo: Brinquedo): void {
+    const itemCarrinho: ItemCarrinho = {
+      id: 0, // o ID será gerado no backend
+      idCarrinho: 4, // ajuste conforme necessário para obter o carrinho atual
+      brinquedo: brinquedo,
+      quantidade: 1
+    };
+
+    this.itemCarrinhosService.adicionarAoCarrinho(itemCarrinho).subscribe(
+      resultado => {
+        console.log('Item adicionado ao carrinho com sucesso', resultado);
+      },
+      erro => {
+        console.error('Erro ao adicionar item ao carrinho', erro);
+      }
+    );
+  }
 }
