405bd1f45e2639a158b816766767621eeb4fcf2b
Icaleb1 <calebfernandes080@gamil.com>
2024-06-17 10:20:17 -0300

    Criação dos módulos e componentes de aluguel e usuario

---
+++ src/app/alugueis/aluguel-detalhe/aluguel-detalhe.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-aluguel-detalhe',
+  standalone: true,
+  imports: [],
+  templateUrl: './aluguel-detalhe.component.html',
+  styleUrl: './aluguel-detalhe.component.scss'
+})
+export class AluguelDetalheComponent {
+
+}
---
+++ src/app/alugueis/aluguel-listagem/aluguel-listagem.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-aluguel-listagem',
+  standalone: true,
+  imports: [],
+  templateUrl: './aluguel-listagem.component.html',
+  styleUrl: './aluguel-listagem.component.scss'
+})
+export class AluguelListagemComponent {
+
+}
