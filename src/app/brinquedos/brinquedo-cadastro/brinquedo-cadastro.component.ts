e4906320cfd622e5c660aea81d9f02b09ac146b9
Icaleb1 <calebfernandes080@gamil.com>
2024-06-18 09:23:11 -0300

    Método de cadastro

--- src/app/brinquedos/brinquedo-cadastro/brinquedo-cadastro.component.ts
+++ src/app/brinquedos/brinquedo-cadastro/brinquedo-cadastro.component.ts
@@ -1,13 +1,46 @@
-import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { BrinquedosService } from '../../shared/service/brinquedo/brinquedos.service';
+import { Brinquedo } from './../../shared/model/brinquedo';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-brinquedo-cadastro',
-  standalone: true,
-  imports: [],
   templateUrl: './brinquedo-cadastro.component.html',
   styleUrl: './brinquedo-cadastro.component.scss'
 })
-export class BrinquedoCadastroComponent {
+export class BrinquedoCadastroComponent implements OnInit{
 
-  
+  public brinquedo: Brinquedo = new Brinquedo();
+
+  constructor(private brinquedoService: BrinquedosService,
+    private router:Router,
+    private route: ActivatedRoute
+  ){}
+
+  ngOnInit(): void {
+
+  }
+
+
+  public cadastrarBrinquedo(){
+    this.brinquedoService.cadastrarBrinquedo(this.brinquedo).subscribe(
+    (resposta) => {
+      Swal.fire('Brinquedo cadastrado com sucesso!', '', 'success'); this.voltar();
+    },
+    (erro) => {
+      Swal.fire('Erro ao salvar o Brinquedo!', erro, 'error');
+    }
+  );
 }
+
+
+voltar() {
+  this.router.navigate(['/brinquedos']);
+}
+
+
+
+}
+
