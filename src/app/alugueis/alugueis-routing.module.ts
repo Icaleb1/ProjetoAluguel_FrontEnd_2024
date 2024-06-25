b425a7b34f4075825b22a1ce54e8553210d0e1a8
EMANUEL-KRASS-DE-SOUZA <emanuelkraussdesouza@gmail.com>
2024-06-25 10:30:28 -0300

    Criação de cadastro de aluguel

--- src/app/alugueis/alugueis-routing.module.ts
+++ src/app/alugueis/alugueis-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AluguelDetalheComponent } from './aluguel-detalhe/aluguel-detalhe.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: AluguelDetalheComponent},
+  {path: "alugar", component: AluguelDetalheComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
