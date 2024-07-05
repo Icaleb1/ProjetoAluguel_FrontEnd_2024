6ad8867676684016f54a9638b0275527a3fae8ca
EMANUEL-KRASS-DE-SOUZA <emanuelkraussdesouza@gmail.com>
2024-07-04 21:19:04 -0300

    Criação do endereço

---
+++ src/app/enderecos/endereco-detalhe/endereco-detalhe.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-endereco-detalhe',
+  standalone: true,
+  imports: [],
+  templateUrl: './endereco-detalhe.component.html',
+  styleUrl: './endereco-detalhe.component.scss'
+})
+export class EnderecoDetalheComponent {
+
+}
---
+++ src/app/enderecos/endereco-listagem/endereco-listagem.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-endereco-listagem',
+  standalone: true,
+  imports: [],
+  templateUrl: './endereco-listagem.component.html',
+  styleUrl: './endereco-listagem.component.scss'
+})
+export class EnderecoListagemComponent {
+
+}
