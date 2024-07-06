db92c6c48ac12fe71e5d1fff935916956bcc17b2
EMANUEL-KRASS-DE-SOUZA <emanuelkraussdesouza@gmail.com>
2024-07-05 23:17:04 -0300

    teste

--- src/app/enderecos/endereco-listagem/endereco-listagem.component.ts
+++ src/app/enderecos/endereco-listagem/endereco-listagem.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-endereco-listagem',
   templateUrl: './endereco-listagem.component.html',
-  styleUrl: './endereco-listagem.component.scss'
+  styleUrls: ['./endereco-listagem.component.scss']
 })
 export class EnderecoListagemComponent {
+  constructor(private router: Router) {}
 
+  cadastrarEndereco() {
+    this.router.navigate(['/home/enderecos/detalhe']);
+  }
 }
